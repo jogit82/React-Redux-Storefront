73ad37fcc73043523485643d65484db5a4085e62
Jo <jo@iMac.kitchenpc.net>
2023-03-25 23:22:43 -0700

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+// Import createStore and combineReducers here.
+import { createStore, combineReducers } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+// Import the slice reducers here.
+import { inventoryReducer } from "../features/inventory/inventorySlice.js";
+import { cartReducer } from "../features/cart/cartSlice.js";
+import { currencyFilterReducer } from "../features/currencyFilter/currencyFilterSlice.js";
+import { searchTermReducer } from "../features/searchTerm/searchTermSlice.js";
+
+// Create and export the store here.
+export const store = createStore(
+  combineReducers({
+    inventory: inventoryReducer,
+    cart: cartReducer,
+    currencyFilter: currencyFilterReducer,
+    searchTerm: searchTermReducer,
+  })
+);
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const container = document.getElementById('root');
-const root = createRoot(container);
+import { App } from "./app/App.js";
+import { store } from "./app/store.js";
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+// Pass state and dispatch props to the <App /> component.
+const render = () => {
+  ReactDOM.render(
+    <App state={store.getState()} dispatch={store.dispatch} />,
+    document.getElementById("root")
+  );
+};
+render();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Subscribe render to the store.
+store.subscribe(render);
